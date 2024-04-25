3d48a2b28fd196bf81856370e1b31c76ec67314c
roy cyril dosado <rcdosado@gmail.com>
2024-04-26 06:29:32 +0800

    add python3 provider to fix treesitter issue that made neotest not finding test functions

--- init.lua
+++ init.lua
@@ -93,6 +93,8 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
 
+vim.g.python3_host_prog = 'c:/python312/python.exe'
+
 local nvim_path = vim.fn.stdpath 'config'
 
 -- [[ Setting options ]]

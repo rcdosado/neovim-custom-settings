613f39adfc0bd8e856401815c8976dbeb2abaedb
roy cyril dosado <rcdosado@gmail.com>
2024-04-30 16:37:05 +0800

    move neotest shortcuts here

--- lua/custom/plugins/nvim-neotest.lua
+++ lua/custom/plugins/nvim-neotest.lua
@@ -1,12 +1,11 @@
 return {
-  {},
   {
     'nvim-neotest/neotest',
     dependencies = {
+      'nvim-treesitter/nvim-treesitter',
       'nvim-neotest/nvim-nio',
       'nvim-lua/plenary.nvim',
       'antoinemadec/FixCursorHold.nvim',
-      'nvim-treesitter/nvim-treesitter',
 
       -- language specific
       'nvim-neotest/neotest-python',
@@ -14,37 +13,63 @@ return {
     -- lazy = true,
     config = function()
       require('neotest').setup {
-        adapters = {
-          require 'neotest-python' {
-            -- runner = 'pytest',
-            -- env = { CI = true },
-            -- cwd = function(path)
-            --   return vim.fn.getcwd()
-            -- end,
-            -- Extra arguments for nvim-dap configuration
-            -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-            dap = { justMyCode = false },
-            -- Command line arguments for runner
-            -- Can also be a function to return dynamic values
-            args = { '--log-level', 'DEBUG' },
-            -- Runner to use. Will use pytest if available by default.
-            -- Can be a function to return dynamic value.
-            runner = 'pytest',
-            -- Custom python path for the runner.
-            -- Can be a string or a list of strings.
-            -- Can also be a function to return dynamic value.
-            -- If not provided, the path will be inferred by checking for
-            -- virtual envs in the local directory and for Pipenev/Poetry configs
-            python = 'python',
-            -- Returns if a given file path is a test file.
-            -- NB: This function is called a lot so don't perform any heavy tasks within it.
-            -- is_test_file = function(file_path) end,
-            -- !!EXPERIMENTAL!! Enable shelling out to `pytest` to discover test
-            -- instances for files containing a parametrize mark (default: false)
-            -- pytest_discover_instances = true,
+        log_level = vim.log.levels.DEBUG,
+        strategies = {
+          integrated = {
+            width = 180,
           },
         },
+        adapters = {
+          require 'neotest-python' {
+            -- args = { '--log-level', 'DEBUG' },
+            runner = 'pytest',
+            python = 'python',
+            dap = { justMyCode = false, console = 'integratedTerminal', subProcess = false },
+          },
+        },
+        status = { virtual_text = true, signs = true },
+        output = { open_on_run = true },
       }
+
+      -- Keymaps
+      vim.keymap.set('n', '<leader>ntf', function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end, { desc = 'Run File' })
+
+      vim.keymap.set('n', '<leader>nra', function()
+        require('neotest').output.open { enter = true, last_run = true }
+      end, { desc = 'Run attached' })
+
+      vim.keymap.set('n', '<leader>nta', function()
+        require('neotest').run.run(vim.loop.cwd())
+      end, { desc = 'Run All Test Files' })
+
+      vim.keymap.set('n', '<leader>ntt', function()
+        require('neotest').run.run()
+      end, { desc = 'Run Nearest' })
+
+      vim.keymap.set('n', '<leader>ntl', function()
+        require('neotest').run.run_last()
+      end, { desc = 'Run Last test' })
+
+      vim.keymap.set('n', '<leader>nss', function()
+        require('neotest').summary.toggle()
+      end, { desc = 'Toggle Summary' })
+
+      vim.keymap.set('n', '<leader>nso', function()
+        require('neotest').output.open { enter = true, auto_close = true }
+      end, { desc = 'Show Output' })
+
+      vim.keymap.set('n', '<leader>nsp', function()
+        require('neotest').output_panel.toggle()
+      end, { desc = 'Toggle Output Panel' })
+
+      vim.keymap.set('n', '<leader>ntx', function()
+        require('neotest').run.stop()
+      end, { desc = 'Stop' })
+
+      vim.keymap.set('n', '[n', '<cmd>lua require("neotest").jump.prev({status = "failed"})<CR>', { silent = true })
+      vim.keymap.set('n', ']n', '<cmd>lua require("neotest").jump.next({status = "failed"})<CR>', { silent = true })
     end,
   },
 }

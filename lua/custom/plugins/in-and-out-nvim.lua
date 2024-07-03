b3cdafc39226e7b029539d3c13a3488156f1a6d9
roy cyril dosado <rcdosado@gmail.com>
2024-07-04 02:55:45 +0800

    add in-and-out plugin

---
+++ lua/custom/plugins/in-and-out-nvim.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'ysmb-wtsg/in-and-out.nvim',
+    config = function()
+      vim.keymap.set('i', '<M-.>', function()
+        require('in-and-out').in_and_out()
+      end)
+    end,
+  },
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -90,6 +90,10 @@ require('lazy').setup({
 
   require 'custom/plugins/egrepify-nvim',
 
+  require 'custom/plugins/in-and-out-nvim',
+
+  -- require 'custom/plugins/my-note-nvim',
+
   -- require 'custom/plugins/ollama-nvim',
   -- require 'custom/plugins/llm',
 

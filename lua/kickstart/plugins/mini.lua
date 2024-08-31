ad19f8bbf3e011c0e087f4caa2cc4851f3f7b70c
webdevpalawan <webdevpalawan@gmail.com>
2024-09-01 06:33:58 +0800

    remove mini.surround
    remove because of key conflicts in checkhealth

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -32,27 +32,6 @@ return {
         },
       }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup {
-        -- defaults
-        -- mappings = {
-        --   add = '_sa', -- Add surrounding in Normal and Visual modes
-        --   delete = '_sd', -- Delete surrounding
-        --   find = '_sf', -- Find surrounding (to the right)
-        --   find_left = '_sF', -- Find surrounding (to the left)
-        --   highlight = '_sh', -- Highlight surrounding
-        --   replace = '_sr', -- Replace surrounding
-        --   update_n_lines = '_sn', -- Update `n_lines`
-        --
-        --   suffix_last = 'l', -- Suffix to search with "prev" method
-        --   suffix_next = 'n', -- Suffix to search with "next" method
-        -- },
-      }
-
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin

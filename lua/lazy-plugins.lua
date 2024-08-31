bc5166189fdba67e326b6d9c0c22731eec4b9167
webdevpalawan <webdevpalawan@gmail.com>
2024-09-01 06:37:29 +0800

    add nvim-surround to replace mini.surround

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -105,6 +105,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
 
   require 'custom/plugins/markview-nvim',
+  require 'custom/plugins/nvim-surround',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.

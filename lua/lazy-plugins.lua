bab6789e66e137b5d524234961319f9435e21e8a
webdevpalawan <webdevpalawan@gmail.com>
2024-09-01 06:34:32 +0800

    add markview-nvim plugin
    this is a new neovim plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -104,6 +104,8 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
 
+  require 'custom/plugins/markview-nvim',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
